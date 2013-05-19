fbfc3fcff0822fa8ff14fd2ce2ba88cca97678c1
Jakub Arnold <darthdeus@gmail.com>
2013-05-19 18:43:16 +0200

    Convert data to json string

--- lib/catsocket_client/publisher.rb
+++ lib/catsocket_client/publisher.rb
@@ -4,7 +4,7 @@ module CatsocketClient
   class Publisher < Struct.new(:url, :api_key)
 
     def publish(channel, data)
-      RestClient.post(url, guid: 0, api_key: api_key, channel: channel, data: data )
+      RestClient.post(url, guid: 0, api_key: api_key, channel: channel, data: data.to_json)
     end
 
   end
