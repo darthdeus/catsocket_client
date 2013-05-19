de2be3eba8e63e8c2011c971252f368cf7303647
Jakub Arnold <darthdeus@gmail.com>
2013-05-19 18:44:47 +0200

    Convert data to json only if it is not already a string

--- lib/catsocket_client/publisher.rb
+++ lib/catsocket_client/publisher.rb
@@ -4,7 +4,8 @@ module CatsocketClient
   class Publisher < Struct.new(:url, :api_key)
 
     def publish(channel, data)
-      RestClient.post(url, guid: 0, api_key: api_key, channel: channel, data: data.to_json)
+      json = data.is_a?(String) ? data : data.to_json
+      RestClient.post(url, guid: 0, api_key: api_key, channel: channel, data: json)
     end
 
   end
